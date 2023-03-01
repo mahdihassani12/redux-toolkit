9968ff80b87adfca3cb0df3a88c75c12d47969e9
Mahdi <hassanim430@gmail.com>
2023-03-01 19:35:10 +0430

    adding multiple action types with one reducer

---
+++ multipleReducers.js
@@ -0,0 +1,91 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const bindActionCreatores = redux.bindActionCreators;
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
+
+const cakeRestocked = (qty = 1) => {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  };
+};
+
+const cakeOrdered = (qty = 1) => {
+  return {
+    type: CAKE_ORDERED,
+    payload: qty,
+  };
+};
+
+const icecreamRestocked = (qty = 1) => {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+};
+
+const icecreamOrdered = (qty = 1) => {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+};
+
+const initialState = {
+  numOfCakes: 10,
+  numOfIcecream: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - action.payload,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIcecream: state.numOfIcecream - action.payload,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIcecream: state.numOfIcecream + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log("Initial State ", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("Update state ", store.getState())
+);
+
+const actions = bindActionCreatores(
+  { cakeOrdered, cakeRestocked, icecreamOrdered, icecreamRestocked },
+  store.dispatch
+);
+actions.cakeOrdered();
+actions.cakeOrdered();
+actions.cakeOrdered();
+actions.cakeRestocked(3);
+
+actions.icecreamOrdered();
+actions.icecreamOrdered();
+actions.icecreamOrdered();
+actions.icecreamRestocked(3);
+
+unsubscribe();
