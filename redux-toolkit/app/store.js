a3868f2bda98bd1fb1cc544af1b5fec552fd3e5d
Mahdi <hassanim430@gmail.com>
2023-03-03 10:48:13 +0430

    adding one more redux toolkit example

--- redux-toolkit/app/store.js
+++ redux-toolkit/app/store.js
@@ -1,9 +1,11 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const cakeReduer = require("../features/Cake/cakeSlice");
+const icecreamReduer = require("../features/IceCream/iceCreamSlice");
 
 const store = configureStore({
   reducer: {
     cake: cakeReduer,
+    icecream: icecreamReduer,
   },
 });
 
--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -1,12 +1,22 @@
 const store = require("./app/store");
-const cakeActions = require("./features/Cake/cakeSlice").cakeActions
+const cakeActions = require("./features/Cake/cakeSlice").cakeActions;
+const icecreamActions = require("./features/IceCream/iceCreamSlice").icecreamActions;
 
 console.log("Initial State ", store.getState());
-const unsubscribe = store.subscribe(() => console.log("Update state, ", store.getState()));
+const unsubscribe = store.subscribe(() =>
+  console.log("Update state, ", store.getState())
+);
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3));
+
+
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(3));
 
 unsubscribe();
